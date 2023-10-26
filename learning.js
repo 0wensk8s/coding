989015890b926cfe006f4fe227e5e7b886972dfc
Owen witherspoon <owitherspoon5@gmail.com>
2023-10-26 11:13:56 -0400

    Added products

--- learning.js
+++ learning.js
@@ -1,1 +1,23 @@
 console.log("Hello World!")
+
+let name="owen"
+
+console.log("alanda")
+console.log(name + " witherspoon ")
+
+
+let x = 1
+console.log(x)
+
+
+1 + 2 + "a"
+   ++x
+   2 + 2
+   console.log
+   
+   99=== "99"
+
+console.log(
+
+"a " + "b"
+)
